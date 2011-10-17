d21babb6af291e2c0d60dc0146b8e11831d4a476
Sean Miller <sean@redwoodstrategic.com>
2011-10-16 21:57:29 -0400

    Add full-ack

--- init.el
+++ init.el
@@ -1,8 +1,16 @@
-
 ;; -- magit ------------------------------------------------------------------
 (add-to-list 'load-path "~/.emacs.d/vendor/magit-1.0.0/")
 (setq exec-path (append exec-path '("/usr/local/git/bin")))
 (require 'magit)
+;; -- end --------------------------------------------------------------------
+
+;; -- full-ack ---------------------------------------------------------------
+(setq exec-path (append exec-path '("/usr/local/bin")))
+(add-to-list 'load-path "~/.emacs.d/vendor/full-ack-0.2.2/")
+(autoload 'ack-same "full-ack" nil t)
+(autoload 'ack "full-ack" nil t)
+(autoload 'ack-find-same-file "full-ack" nil t)
+(autoload 'ack-find-file "full-ack" nil t)
 ;; -- end --------------------------------------------------------------------
 
 ;; -- rows and columns -------------------------------------------------------
