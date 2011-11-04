66352cc9f13c5d024301392d298aca0e8fce704c
Sean Miller <sean@redwoodstrategic.com>
2011-11-04 13:03:01 -0400

    * init.el: auto-load vendor/ directories; add themes/zenburn; fix kbd conflict

--- init.el
+++ init.el
@@ -1,16 +1,12 @@
 ;; == internals ==============================================================
 
-;; -- directories ------------------------------------------------------------
+;; -- load everything from dotfiles-init-dir ---------------------------------
 (setq init-file (or load-file-name buffer-file-name))
 (setq dotfiles-dir (file-name-directory init-file))
-;; -- end --------------------------------------------------------------------
-
-;; -- load everything from dotfiles-init-dir ---------------------------------
 (setq dotfiles-init-dir (expand-file-name "init.d" dotfiles-dir))
 (if (file-exists-p dotfiles-init-dir)
     (dolist (file (directory-files dotfiles-init-dir t "\\.el$"))
       (load file)))
-;; -- end --------------------------------------------------------------------
 
 ;; -- disable stuff ----------------------------------------------------------
 (setq make-backup-files nil)
@@ -19,50 +15,53 @@
 (tool-bar-mode -1)
 (setq inhibit-startup-message t)
 (fset 'yes-or-no-p 'y-or-n-p)
-;; -- end --------------------------------------------------------------------
 
 ;; -- rows and columns -------------------------------------------------------
 (setq line-number-mode t)
 (setq column-number-mode t)
-
 (global-linum-mode t)
 (setq linum-format "%4d ")
-
 (setq-default fill-column 80)
 (add-hook 'text-mode-hook 'longlines-mode)
-;; -- end --------------------------------------------------------------------
 
 ;; -- whitespace -------------------------------------------------------------
 (require 'whitespace)
 (add-hook 'ruby-mode-hook 'whitespace-mode)
 (add-hook 'js-mode-hook 'whitespace-mode)
-;; -- end --------------------------------------------------------------------
 
 ;; -- coding -----------------------------------------------------------------
 (setq tags-table-list
       '("/Users/sean/projects/qs-core/TAGS"))
-
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.yml$" . ruby-mode))
-;; -- end --------------------------------------------------------------------
 
 ;; -- custom key bindings ----------------------------------------------------
 ;; because ruby-mode overrides C-M-p / C-M-n defaults
 (global-set-key (kbd "M-g M-[") 'backward-list)
 (global-set-key (kbd "M-g M-]") 'forward-list)
-;; -- end --------------------------------------------------------------------
 
 ;; == third-party packages ===================================================
 
-;; -- magit ------------------------------------------------------------------
-(add-to-list 'load-path "~/.emacs.d/vendor/magit-1.0.0/")
-(setq exec-path (append exec-path '("/usr/local/git/bin")))
-(require 'magit)
-(global-set-key (kbd "M-g M-m") 'magit-status)
+(setq themes-dir (expand-file-name "themes" dotfiles-dir))
+(add-to-list 'custom-theme-load-path themes-dir)
+(load-theme 'zenburn t)
 
+(setq vendor-dir (expand-file-name "vendor" dotfiles-dir))
+(dolist (project (directory-files vendor-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
+
+(require 'magit)
+(setq exec-path (append exec-path '("/usr/local/git/bin")))
+(global-set-key (kbd "M-g M-m") 'magit-status)
+;; defaults conflict with window-numbering
+(global-set-key (kbd "M-g 1") 'magit-show-level-1)
+(global-set-key (kbd "M-g 2") 'magit-show-level-2)
+(global-set-key (kbd "M-g 3") 'magit-show-level-3)
+(global-set-key (kbd "M-g 4") 'magit-show-level-4)
 ;; http://readystate4.com/2011/02/22/emacs-changing-magits-default-diff-colors/
 (eval-after-load 'magit
   '(progn
@@ -70,45 +69,26 @@
      (set-face-foreground 'magit-diff-del "red3")
      (when (not window-system)
        (set-face-background 'magit-item-highlight "black"))))
-;; -- end --------------------------------------------------------------------
 
-;; -- full-ack ---------------------------------------------------------------
-(add-to-list 'load-path "~/.emacs.d/vendor/full-ack-0.2.2")
-(setq exec-path (append exec-path '("/usr/local/bin")))
 (autoload 'ack-same "full-ack" nil t)
 (autoload 'ack "full-ack" nil t)
 (autoload 'ack-find-same-file "full-ack" nil t)
 (autoload 'ack-find-file "full-ack" nil t)
-;; -- end --------------------------------------------------------------------
+(setq exec-path (append exec-path '("/usr/local/bin")))
 
-;; -- iy-go-to-char ----------------------------------------------------------
-(add-to-list 'load-path "~/.emacs.d/vendor/iy-go-to-char/")
 (require 'iy-go-to-char)
 (global-set-key (kbd "M-n") 'iy-go-to-char)
 (global-set-key (kbd "M-N") 'iy-go-to-char-backward)
-;; -- end --------------------------------------------------------------------
 
-;; -- inf-ruby ---------------------------------------------------------------
-(add-to-list 'load-path "~/.emacs.d/vendor/inf-ruby-2.2.2/")
 (require 'inf-ruby)
-;; -- end --------------------------------------------------------------------
 
-;; -- unbound ----------------------------------------------------------------
-(add-to-list 'load-path "~/.emacs.d/vendor/unbound-0.1/")
 (require 'unbound)
-;; -- end --------------------------------------------------------------------
 
-;; -- haml-mode --------------------------------------------------------------
-(add-to-list 'load-path "~/.emacs.d/vendor/haml-mode/")
 (require 'haml-mode)
 (add-hook 'haml-mode-hook
                   '(lambda ()
                          (setq indent-tabs-mode nil)
                          (define-key haml-mode-map "\C-m" 'newline-and-indent)))
-;; -- end --------------------------------------------------------------------
 
-;; -- windows-numbering ------------------------------------------------------
-(add-to-list 'load-path "~/.emacs.d/vendor/window-numbering/")
 (require 'window-numbering)
 (window-numbering-mode 1)
-;; -- end --------------------------------------------------------------------
