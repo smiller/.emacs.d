574d964492513228c48d3cac16321ef935d8dc94
Sean Miller <sean@redwoodstrategic.com>
2011-12-01 00:21:46 -0500

    Add typopunct-ellipsis

--- init.el
+++ init.el
@@ -126,3 +126,21 @@
 
 (require 'typopunct)
 (typopunct-change-language 'english t)
+;; http://www.emacswiki.org/emacs/TypographicalPunctuationMarks
+(defconst typopunct-ellipsis (decode-char 'ucs #x2026))
+    (defun typopunct-insert-ellipsis-or-middot (arg)
+      "Change three consecutive dots to a typographical ellipsis mark."
+      (interactive "p")
+      (cond
+       ((and (= 1 arg)
+             (eq (char-before) ?^))
+        (delete-char -1)
+        (insert typopunct-middot))
+       ((and (= 1 arg)
+             (eq this-command last-command)
+             (looking-back "\\.\\."))
+        (replace-match "")
+        (insert typopunct-ellipsis))
+       (t
+        (self-insert-command arg))))
+    (define-key typopunct-map "." 'typopunct-insert-ellipsis-or-middot)
