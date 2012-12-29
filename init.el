b91dc91981ef85d08e1619f7b9073d12ae975599
Sean Miller <sean@seanmiller.ca>
2012-12-29 18:40:57 -0500

    init.el: open .md files in markdown-mode

--- init.el
+++ init.el
@@ -72,6 +72,9 @@
 (defadvice ansi-term (after advise-ansi-term-coding-system)
     (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
 (ad-activate 'ansi-term)
+
+(setq auto-mode-alist
+  (cons '("\\.md" . markdown-mode) auto-mode-alist))
 ;; == third-party packages ===================================================
 
 (setq themes-dir (expand-file-name "themes" dotfiles-dir))
